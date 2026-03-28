8b6224a3b25a139954b28ddd82f7cecb7ddf03be
Soumyadip Samanta <samantasoumyadip1998@gmail.com>
2026-03-29 02:24:08 +0530

    Header part of the new website

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
 import ReactDOM from "react-dom/client";
 
-const Title = () => (
-    <h1 className="head" tabIndex="5">
-        Namaste React using JSX 🚀
-    </h1>
-);
+const Header = () => {
+    return (
+        <div className = "header">
+            <div>
+                <img className="Logo" src="https://www.psdtemplatedesign.com/wp-content/uploads/edd/2017/12/logo-design.jpg" />
+            </div>
+            <div className ="navbar">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
 
-const HeadingComponent = () => (
-    <div id="Container">
-        <Title />
-        <h2 className="heading">Namaste react Functional Component</h2>
-    </div>
-);
+const AppLayout = () => {
+    return (
+        <div className="app"> 
+            <Header />
+        </div>
+    );
+};    
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
