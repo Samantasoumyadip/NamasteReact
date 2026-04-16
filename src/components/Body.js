e5f0bddc2f9aa6ad018a96873913c64420846497
Soumyadip Samanta <samantasoumyadip1998@gmail.com>
2026-04-17 02:24:14 +0530

    Feature of filtering restaurants using hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,24 @@
 import Restaurant from "./Restaurant";
 import resList from "../utils/reslist";
+import { useState } from "react";
+
 
 const Body = () => {
+
+    const [listOfRestaurants, setListOfRestaurants] = useState(resList);
     return (
         <div className="body">
-        <div className="Search"> Search </div>
+        <div className="Filter">  
+        <button className="filter-btn" 
+        onClick={() => {
+            const filteredList = listOfRestaurants.filter(restaurant => restaurant.info.avgRating > 4.4);
+            setListOfRestaurants(filteredList);
+        }}
+        >Top Rated Restaurants</button>
+        </div>
         <div className="res-container"> 
         {
-        resList.map(restaurant => (<Restaurant resData = {restaurant} key={restaurant.info.id} />
+        listOfRestaurants.map(restaurant => (<Restaurant resData = {restaurant} key={restaurant.info.id} />
         ))}
         </div>
         </div>
