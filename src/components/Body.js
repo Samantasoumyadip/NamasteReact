e563ed4bd81689cd9a3bc45af349843cdb094f3c
Soumyadip Samanta <samantasoumyadip1998@gmail.com>
2026-05-28 21:34:30 +0530

    Using live api and using useeffect hook.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,28 @@
 import Restaurant from "./Restaurant";
 import resList from "../utils/reslist";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 const Body = () => {
 
     const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.5656801&lng=88.5143351&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        console.log(json);
+        setListOfRestaurants(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+    }
+
     return (
         <div className="body">
         <div className="Filter">  
         <button className="filter-btn" 
         onClick={() => {
-            const filteredList = listOfRestaurants.filter(restaurant => restaurant.info.avgRating > 4.4);
+            const filteredList = listOfRestaurants.filter(restaurant => restaurant.info.avgRating > 4.2);
             setListOfRestaurants(filteredList);
         }}
         >Top Rated Restaurants</button>
