844d500205413a8bc56f8ad0f508db38de059a3b
Soumyadip Samanta <samantasoumyadip1998@gmail.com>
2026-06-13 19:47:01 +0530

    Login-logout button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,30 @@
 import { LOGO_URL } from "../utils/constants";  
-
+import { useState } from "react";
 
 const Header = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState("Login");
+
     return (
-        <div className = "header">
+        <div className="header">
             <div>
-                <img className="Logo" src={LOGO_URL} />
+                <img className="Logo" src={LOGO_URL} alt="Logo" />
             </div>
-            <div className ="navbar">
+            <div className="navbar">
                 <ul>
                     <li>Home</li>
                     <li>About</li>
                     <li>Contact</li>
                     <li>Cart</li>
+                    <li>
+                        <button
+                            className="login"
+                            onClick={() =>
+                                setIsLoggedIn(isLoggedIn === "Login" ? "Logout" : "Login")
+                            }
+                        >
+                            {isLoggedIn}
+                        </button>
+                    </li>
                 </ul>
             </div>
         </div>
